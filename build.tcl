308bc607ef69909aca09f17f95d69f55e816db88
Marco Pagani <marco.pag@outlook.com>
2021-05-17 12:04:53 +0200

    64-bit addresses

--- build.tcl
+++ build.tcl
@@ -4,7 +4,6 @@ set num_slots 4
 set name "hw_task_memcpy"
 
 open_project $name
-
 # HW-task files
 add_files srcs/hw_task.cpp
 add_files srcs/hw_mod.cpp
@@ -13,7 +12,9 @@ set i 0
 while {$i < $num_slots} {
   set_top hw_task_$i
   open_solution "solution_$i"
-  set_part {xc7z020clg400-1} -tool vivado
+  set_part {xczu9eg-ffvb1156-2-i} -tool vivado
+  # 64-bit AXI-M address capability
+  config_interface -m_axi_addr64
   create_clock -period 10 -name default
   csynth_design
   export_design -format ip_catalog -description "${name}_${i}" -display_name "${name}_${i}"
